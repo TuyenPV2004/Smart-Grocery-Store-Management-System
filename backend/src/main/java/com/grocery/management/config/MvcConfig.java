b56d7617b0e001c79c19bd0087e92a32789fa64b
TuyenPV2004 <phamtuyen13112004@gmail.com>
2026-02-19 13:35:48 +0700

    feat: introduce Point of Sale (POS) page and backend MVC configuration.

--- backend/src/main/java/com/grocery/management/config/MvcConfig.java
+++ backend/src/main/java/com/grocery/management/config/MvcConfig.java
@@ -17,15 +17,12 @@ public class MvcConfig implements WebMvcConfigurer {
 
     private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
         Path uploadDir = Paths.get(dirName);
-        // Sửa đoạn này: Sử dụng toUri().toString() để đảm bảo đường dẫn chuẩn xác
         String uploadPath = uploadDir.toFile().getAbsolutePath();
 
         if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
 
         registry.addResourceHandler("/" + dirName + "/**")
                 .addResourceLocations("file:///" + uploadPath + "/"); 
-                // Lưu ý: Thêm đủ 3 dấu gạch chéo /// sau file: để chạy tốt trên Windows
-        // Trong phương thức addResourceHandlers
         registry.addResourceHandler("/product-images/**")
                 .addResourceLocations("file:product-images/");
     }
